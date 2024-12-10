0ad2472059de7eca641499c8522aad00ea833b23
Fabriziuo <fabriziodejesusalmeidamora@gmail.com>
2024-12-09 22:43:20 -0400

    Probando los elementos

--- script/app.js
+++ script/app.js
@@ -1,1 +1,4 @@
 // Hay que hacer los movimientos de las piezas
+const boardSquares = document.getElementsByClassName("square");
+const pieces = document.getElementsByClassName("piece");
+const piecesImages = document.getElementsByTagName("img");
